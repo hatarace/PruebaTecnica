8e6b5e9723b78d2b076e9a25f9b70ce46cbbba6a
hatarace <halutaracena@gmail.com>
2026-01-22 17:05:36 -0600

    Add files via upload
    Creacion de proyecto frontend

---
+++ FrontEnd/src/app/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [MatGridListModule],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {
+
+}
